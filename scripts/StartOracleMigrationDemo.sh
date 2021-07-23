315f391b76d27aa75ce30a3938f4d2a0727a054b
Aaron Aldrich <a.aldrich@gmail.com>
2021-07-23 15:54:34 -0400

    updates readme to match demo process & script cleanup

--- scripts/StartOracleMigrationDemo.sh
+++ scripts/StartOracleMigrationDemo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Set environment variables for deployment
+# TODO: Adjust these to avoid overlap between ARO cluster and Oracle Source
 # azure
 AZR_RESOURCE_LOCATION=eastus
 AZR_RESOURCE_GROUP=DemoOracleMigration
@@ -18,26 +19,36 @@ az group create --name $AZR_RESOURCE_GROUP --location $AZR_RESOURCE_LOCATION
 
 # Setup Legacy Environment of MedRec application
 # Create Oracle Database VM
-az vm create ^
-    --resource-group $AZR_RESOURCE_GROUP ^
-    --name vmoracle19c ^
-    --image Oracle:oracle-database-19-3:oracle-database-19-0904:latest ^
-    --size Standard_DS2_v2 ^
-    --admin-username azureuser ^
-    --generate-ssh-keys ^
-    --public-ip-address-allocation static ^
-    --public-ip-address-dns-name vmoracle19c
+# The static DNS name causes some challenges, ran into a collision already. Is this something
+# we can retrieve instead of set? It returns JSON eventually so probably.
+
+az vm create \
+    --resource-group $AZR_RESOURCE_GROUP \
+    --name vmoracle19c \
+    --image Oracle:oracle-database-19-3:oracle-database-19-0904:latest \
+    --size Standard_DS2_v2 \
+    --admin-username azureuser \
+    --generate-ssh-keys \
+    --public-ip-address-allocation static \
+    --public-ip-sku Standard \
+    --public-ip-address-dns-name vmoracle19c 
 
 # Create and attach a new disk for Oracle Datafiles
-az vm disk attach --name oradata01 --new --resource-group $AZR_RESOURCE_GROUP --size-gb 64 --sku StandardSSD_LRS --vm-name vmoracle19c
+az vm disk attach \
+    --name oradata01 \
+    --new \
+    --resource-group $AZR_RESOURCE_GROUP \
+    --size-gb 64 \
+    --sku StandardSSD_LRS \
+    --vm-name vmoracle19c
 
 # Open ports for connectivity
-az network nsg rule create ^
-    --resource-group $AZR_RESOURCE_GROUP ^
-    --nsg-name vmoracle19cNSG ^
-    --name allow-oracle ^
-    --protocol tcp ^
-    --priority 1001 ^
+az network nsg rule create \
+    --resource-group $AZR_RESOURCE_GROUP \
+    --nsg-name vmoracle19cNSG \
+    --name allow-oracle \
+    --protocol tcp \
+    --priority 1001 \
     --destination-port-range 1521
 
 # Install MedRecDDL onto Oracle Database VM
